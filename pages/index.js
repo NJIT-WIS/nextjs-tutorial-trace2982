69baea8fbf950a8bad4fcd5ba722703e59f96d89
Tracy Harrison <144746597+trace2982@users.noreply.github.com>
2023-11-16 09:23:23 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title><Create Next App</title>
+        <title><Create Next App></title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
       <section className={utilStyles.headingMd}>
