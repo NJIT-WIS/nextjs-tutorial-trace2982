17513c3798df444ae880b1a54afd640123d6e743
Tracy Harrison <144746597+trace2982@users.noreply.github.com>
2023-11-17 12:14:50 -0500

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-import React from 'react';
 
 const name = 'Tracy Harrison';
 export const siteTitle = 'Next.js Sample Website';
