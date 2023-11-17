ce36b0029fd095eea9b8d9a66c671ad72c1d3ae5
Tracy Harrison <144746597+trace2982@users.noreply.github.com>
2023-11-17 10:07:52 -0500

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -23,7 +23,7 @@ export default function Layout({ children, home }) {
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="linkedin:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
         {home ? (
@@ -47,7 +47,7 @@ export default function Layout({ children, home }) {
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
-                alt=""
+                alt={name}
               />
             </Link>
             <h2 className={utilStyles.headingLg}>
